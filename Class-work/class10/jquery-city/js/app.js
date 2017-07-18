297109efd7ddafb295ac07a48af901d846b6cf1d
LeeKottner <aekottner@gmail.com>
2017-07-17 22:28:48 -0400

    Classwork717

---
+++ Class-work/class10/jquery-city/js/app.js
@@ -0,0 +1,12 @@
+// 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
+
+// work on this
+
+$(".thumb").click(function(){
+	var thumbSrc $(this).attr("src") 
+	$(this.attr("src"));
+
+	
+});
+
+// 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
