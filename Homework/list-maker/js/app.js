85fc96e8cdd796390394bba4dc6fbca9666dfaee
Lee Kottner <aekottner@gmail.com>
2017-08-08 17:07:49 -0400

    Listmaker 8-8

--- Homework/list-maker/js/app.js
+++ Homework/list-maker/js/app.js
@@ -10,11 +10,14 @@
 // Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user (hint: use an if/else statement)
 // Legendary Bonus: Remove individual <li> elements when they are clicked (hint: use $(this) and .remove())
 
-$(document).ready(function () {
-
+$(document).ready(function() {
 	$(#clickme).click() {
-		var newItem = ($(#item).val());
+		var newItem = $("#item").val();
+		function appendItem (item) {
+		$("#list").append("<li>" + item + "</li>");
 	}
+
+	$("#clickme").val("");
 
 	// 1. Attach a .click() event to #clickme; attach an anonymous function to the .click()
 	// 1A. This anonymous function should create a variable, newItem equal to the value of #item
@@ -22,4 +25,6 @@ $(document).ready(function () {
 	// 1C. This anonymous function should then clear the text inside of #item
 
 	// 2. Write the appendItem function here
-})
+
+
+}
