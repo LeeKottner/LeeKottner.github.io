8f07affc33d33a7e7ad966d265c948b2b4c77ed2
LeeKottner <aekottner@gmail.com>
2017-08-21 18:33:30 -0400

    FInal final final Argh.

--- final-project-ann/js/ahz-script.js
+++ final-project-ann/js/ahz-script.js
@@ -1,6 +1,7 @@
 
 
 // overlay photos
+
 $("#bldg-img-wrapper").mouseenter(function() { 
   $("#bldg-overlay").show();  
    });
@@ -9,16 +10,29 @@ $("#bldg-img-wrapper").mouseleave(function() {
  }); 
 
 
-// Get modal, autfill password
+// Get modal, autofill password
 
 $(document).ready(function(){
-	var modal = document.getElementById("#login");	
+	var modal = $("#login");	
   	$(".secret-login").click(function(){
-    var modal=$(this).attr("#login");
-    alert(modal);
-//     $("#psw").click(function() {
-//   		$("#psw").autofill("Allow us...");
-//   	});
+    modal.show();
+    // var filler = {
+    // 	"name": "Allow us..."
+    // }
+    // $("#psw").click(function(){
+  		// $("#psw").autofill(function(filler);
+  	// });
 	});
 
 });
+
+
+
+// $(document).ready(function(){
+// 	var modal = document.getElementById('login');
+// 	// Get the button that opens the modal
+// 	var open = document.getElementByClass("secret-login");
+// 	// When the user clicks on the button, open the modal 
+// 	open.onclick = function() {
+//     	modal.style.display = "block";
+// }})
