04549844efa775a93d296af5ffd32f34acd0ef0e
Lee Kottner <aekottner@gmail.com>
2017-08-21 14:53:57 -0400

    Last ditch effort 8/21

--- final-project-ann/js/ahz-script.js
+++ final-project-ann/js/ahz-script.js
@@ -1,70 +1,24 @@
 
 
 // overlay photos
-$("#bldg-img-wrapper").mouseenter(function() {
-  $("#bldg-overlay").show(); 
-}).mouseleave(function() {
-  $("#bldg-overlay").hide();
-});
+$("#bldg-img-wrapper").mouseenter(function() { 
+  $("#bldg-overlay").show();  
+   });
+$("#bldg-img-wrapper").mouseleave(function() { 
+   $("#bldg-overlay").hide(); 
+ }); 
 
 
-
-var modal = document.getElementById('.modal');
+// Get modal, autfill password
 
 $(document).ready(function(){
-  $('.secret-login').click(function(){
-    var modal=$(this).attr('.modal');
+	var modal = document.getElementById("#login");	
+  	$(".secret-login").click(function(){
+    var modal=$(this).attr("#login");
     alert(modal);
-  });
+//     $("#psw").click(function() {
+//   		$("#psw").autofill("Allow us...");
+//   	});
+	});
+
 });
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == login) {
-//         modal.style.display = "none";
-//     }
-// }
-
-// $("psw").val("**************");
-
-
-// // // // prefill form
-
-// // after the page elements are all loaded, then run the script
-// // Set the input field with unique ID #email to a value
-// // Set the input field with unique ID #name
-
-
-
-
-
-// on rollover  
-//      <img src="images/a-z_bldg.png" id="bldg-img">
-//  replace img with
-//      <img src="images/acme-zenith-overlay.png">
-//  play 64544__benboncan__tawny-owls-2.wav
-
-// on click 
-//  <img src="images/a-z_bldg.png" id="bldg-img">
-//  display username and password popup placeholder text = "Sign in"
-//      play 64544__benboncan__tawny-owls-2.wav soundfile
-//      on text entry
-//          sequence
-//              fill "password" box with "**********"
-//              transition to ahz-index.html
-
-// on click 
-//  <a href="" id="secret-login">Your Account</a>
-//  display username and password popup placeholder text = "Sign in"
-//      on text entry
-//          sequence
-//              fill "password" box with "**********"
-//              transition to ahz-index.html
-
-// on click 
-//  <a href="" id="secret-login">Your Account</a>
-//  display username and password popup placeholder text = "Sign in"
-//      on text entry
-//          sequence
-//              fill "password" box with "**********"
-//              transition to ahz-index.html
